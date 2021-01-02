0ee7420dcc728bfceca837b5187665961be7e940
Hiromune Ito <muniere.fujiwara@gmail.com>
2021-01-02 18:39:00 +0900

    Use python3 instead of python

--- vake/cli/__parser__.py
+++ vake/cli/__parser__.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 from .__action__ import Action
 from .__target__ import Target
@@ -14,24 +15,11 @@ class Parser:
         :param args: Arguments
         :return: Context
         """
-        if sys.version_info >= (2, 7, 0):
-            return cls.__parse_argparse(args)
-        else:
-            return cls.__parse_optparse(args)
-
-    @classmethod
-    def options(cls):
-        """
-        Get possible options
-        """
-        if sys.version_info >= (2, 7, 0):
-            return cls.__make_argparser()._optionals._group_actions
-        else:
-            return list()
-
-    @classmethod
-    def __parse_argparse(cls, args):
-        parser = cls.__make_argparser()
+        parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog, width=240))
+        parser.add_argument("-n", "--dry-run", dest="dry_run", action="store_true", help="Do not execute commands actually")
+        parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Show verbose messages")
+        parser.add_argument("action", type=str, help="Action to perform: (%s)" % "|".join(Action.values()))
+        parser.add_argument("target", type=str, nargs="*", help="Target for action: (%s)" % "|".join(Target.values()))
 
         namespace = parser.parse_args(args)
 
@@ -42,39 +30,3 @@ class Parser:
         context.verbose = namespace.verbose
 
         return context
-
-    @classmethod
-    def __parse_optparse(cls, args):
-        parser = cls.__make_optparser()
-
-        (opts, argv) = parser.parse_args(args)
-
-        context = Context()
-        context.action = argv[0]
-        context.targets = argv[1:] or ["dotfile", "binfile"]
-        context.dry_run = opts.dry_run
-        context.verbose = opts.verbose
-
-        return context
-
-    @classmethod
-    def __make_argparser(cls):
-        import argparse
-
-        parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog, width=240))
-        parser.add_argument("-n", "--dry-run", dest="dry_run", action="store_true", help="Do not execute commands actually")
-        parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Show verbose messages")
-        parser.add_argument("action", type=str, help="Action to perform: (%s)" % "|".join(Action.values()))
-        parser.add_argument("target", type=str, nargs="*", help="Target for action: (%s)" % "|".join(Target.values()))
-
-        return parser
-
-    @classmethod
-    def __make_optparser(cls):
-        import optparse
-
-        parser = optparse.OptionParser(usage="Usage: %prog [options] <command> [<target> ...]")
-        parser.add_option("-n", "--dry-run", dest="dry_run", action="store_true", help="Do not execute commands actually")
-        parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="Show verbose messages")
-
-        return parser
