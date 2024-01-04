0c79e0c6496123c7027607721d4f693998e49fa3
Hiromune Ito <muniere.fujiwara@gmail.com>
2024-01-04 11:47:07 +0900

    Add missing action to install Vim-Plug for Neovim

--- app/vault.ts
+++ app/vault.ts
@@ -163,6 +163,18 @@ export const NeovimCookBook = new CookBook({
       dst: HomeLayout.config().join("nvim/"),
     }),
   ],
+  activate: async (options: shell.CallOptions) => {
+    const url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
+    const path = HomeLayout.data().join("nvim/site/autoload/plug.vim");
+
+    const stat = await Result.runAsyncOr(() => path.stat());
+    if (stat) {
+      options.logger?.info(`File already exists: ${path}`);
+      return;
+    }
+
+    await shell.curl(url, { ...options, output: path });
+  },
 });
 
 export const GitCookBook = new CookBook({
