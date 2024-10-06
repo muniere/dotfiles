b54d3fd148c9d958c4db105224ed32cc0d5e6eb9
Hiromune Ito <muniere.fujiwara@gmail.com>
2024-10-06 21:07:40 +0900

    Separate cookbook for iTerm

--- app/vault.ts
+++ app/vault.ts
@@ -1,7 +1,6 @@
 import { Result } from "../lib/lang.ts";
 import { HomeLayout } from "../lib/layout.ts";
 import { Path } from "../lib/path.ts";
-import { PlistBuddy } from "../lib/plist.ts";
 import { CookBook, PrefSpec } from "../lib/schema.ts";
 
 import * as shell from "../lib/shell.ts";
@@ -12,6 +11,7 @@ export { BinCookBook } from "../vault/bin/vault.ts";
 export { DockerCookBook } from "../vault/docker/vault.ts";
 export { GitCookBook } from "../vault/git/vault.ts";
 export { GitHubCookBook } from "../vault/gh/vault.ts";
+export { iTermCookBook } from "../vault/iterm/vault.ts";
 export { NeovimCookBook } from "../vault/nvim/vault.ts";
 export { NodeCookBook } from "../vault/node/vault.ts";
 export { PythonCookBook } from "../vault/python/vault.ts";
@@ -83,49 +83,6 @@ export const GradleCookBook = new CookBook({
       autoclean: false,
     }),
   ],
-});
-
-export const iTermCookBook = new CookBook({
-  name: "iTermCookBook",
-  prefs: [],
-  activate: async (options: shell.CallOptions) => {
-    const path = new Path("~/Library/Preferences/com.googlecode.iterm2.plist").expandHome();
-
-    const stat = await Result.runAsyncOr(() => path.lstat());
-    if (!stat) {
-      options.logger?.info("iTerm 2 not installed yet. skip.");
-      return;
-    }
-
-    const buddy = new PlistBuddy({
-      path: path.expandHome(),
-    });
-
-    {
-      const key = "New Bookmarks:0:Use Non-ASCII Font";
-      const value = true;
-
-      const result = await buddy.getBoolean(key);
-      if (result === value) {
-        options.logger?.info(`Nerd Font already enabled: ${value}`);
-      } else {
-        await buddy.setBoolean(key, value, options);
-      }
-    }
-
-    {
-      const key = "New Bookmarks:0:Non Ascii Font";
-      const value = "HackNerdFontComplete-Regular 12";
-
-      const result = await buddy.getString(key);
-      if (result === value) {
-        options.logger?.info(`Nerd Font already configured: ${value}`);
-      } else {
-        await buddy.setString(key, value, options);
-      }
-    }
-  },
-  platforms: ["darwin"],
 });
 
 export const XcodeCookBook = new CookBook({
---
+++ vault/iterm/vault.ts
@@ -0,0 +1,48 @@
+import { Result } from "../../lib/lang.ts";
+import { Path } from "../../lib/path.ts";
+import { PlistBuddy } from "../../lib/plist.ts";
+import { CookBook } from "../../lib/schema.ts";
+
+import * as shell from "../../lib/shell.ts";
+
+export const iTermCookBook = new CookBook({
+  name: "iTermCookBook",
+  activate: async (options: shell.CallOptions) => {
+    const path = new Path("~/Library/Preferences/com.googlecode.iterm2.plist").expandHome();
+
+    const stat = await Result.runAsyncOr(() => path.lstat());
+    if (!stat) {
+      options.logger?.info("iTerm 2 not installed yet. skip.");
+      return;
+    }
+
+    const buddy = new PlistBuddy({
+      path: path.expandHome(),
+    });
+
+    {
+      const key = "New Bookmarks:0:Use Non-ASCII Font";
+      const value = true;
+
+      const result = await buddy.getBoolean(key);
+      if (result === value) {
+        options.logger?.info(`Nerd Font already enabled: ${value}`);
+      } else {
+        await buddy.setBoolean(key, value, options);
+      }
+    }
+
+    {
+      const key = "New Bookmarks:0:Non Ascii Font";
+      const value = "HackNerdFontComplete-Regular 12";
+
+      const result = await buddy.getString(key);
+      if (result === value) {
+        options.logger?.info(`Nerd Font already configured: ${value}`);
+      } else {
+        await buddy.setString(key, value, options);
+      }
+    }
+  },
+  platforms: ["darwin"],
+});
