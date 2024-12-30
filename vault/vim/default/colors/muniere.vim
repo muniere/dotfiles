0c9d4314843c36e2a38b4b09d62db6e01a37ab74
Hiromune Ito <muniere.fujiwara@gmail.com>
2024-12-31 00:07:13 +0900

    Generate color scheme by script

--- vault/vim/default/colors/muniere.vim
+++ vault/vim/default/colors/muniere.vim
@@ -1,58 +1,58 @@
 let g:colors_name="muniere"
 
-highlight SpecialKey ctermfg=4 term=bold
-highlight NonText ctermfg=12 term=bold
-highlight Directory ctermfg=4 term=bold
-highlight ErrorMsg ctermfg=15 ctermbg=1 term=standout
+highlight SpecialKey ctermfg=4 term=bold cterm=bold
+highlight NonText ctermfg=12 term=bold cterm=bold
+highlight Directory ctermfg=4 term=bold cterm=bold
+highlight ErrorMsg ctermfg=15 ctermbg=1 term=standout cterm=standout
 highlight IncSearch term=reverse cterm=reverse
-highlight Search ctermbg=11 term=reverse
-highlight MoreMsg ctermfg=2 term=bold 
+highlight Search ctermbg=11 term=reverse cterm=reverse
+highlight MoreMsg ctermfg=2 term=bold cterm=bold
 highlight ModeMsg term=bold cterm=bold
-highlight LineNr ctermfg=130 term=underline 
-highlight CursorLineNr ctermfg=130 term=bold 
-highlight Question ctermfg=2 term=standout 
-highlight StatusLine term=bold,reverse cterm=bold,reverse 
-highlight StatusLineNC term=reverse cterm=reverse 
+highlight LineNr ctermfg=130
+highlight CursorLineNr ctermfg=130 term=bold cterm=bold
+highlight Question ctermfg=2 term=standout cterm=standout
+highlight StatusLine term=bold,reverse cterm=bold,reverse
+highlight StatusLineNC term=reverse cterm=reverse
 highlight VertSplit term=reverse cterm=reverse
-highlight Title ctermfg=5 term=bold 
-highlight Visual ctermbg=7 term=reverse
+highlight Title ctermfg=5 term=bold cterm=bold
+highlight Visual ctermbg=7 term=reverse cterm=reverse
 highlight VisualNOS term=bold,underline cterm=bold,underline
-highlight WarningMsg ctermfg=1 term=standout 
-highlight WildMenu ctermfg=0 ctermbg=11 term=standout 
-highlight Folded ctermfg=0 ctermbg=15 term=standout 
-highlight FoldColumn ctermfg=0 ctermbg=15 term=standout 
-highlight DiffAdd ctermfg=0 ctermbg=2 term=bold 
-highlight DiffChange ctermfg=0 ctermbg=3 term=bold 
-highlight DiffDelete ctermfg=0 ctermbg=6 term=bold 
-highlight DiffText ctermfg=0 ctermbg=7 term=reverse cterm=bold 
-highlight SignColumn ctermbg=0 cterm=NONE
-highlight Conceal ctermfg=7 ctermbg=242 
-highlight SpellBad ctermbg=224 term=reverse 
-highlight SpellCap ctermbg=81 term=reverse 
-highlight SpellRare ctermbg=225 term=reverse 
-highlight SpellLocal ctermbg=14 term=underline 
-highlight Pmenu ctermfg=0 ctermbg=225 
-highlight PmenuSel ctermfg=0 ctermbg=248 
-highlight PmenuSbar ctermbg=248 
-highlight PmenuThumb ctermbg=0 
-highlight TabLine ctermfg=0 ctermbg=7 term=underline cterm=underline 
-highlight TabLineSel term=bold cterm=bold 
-highlight TabLineFill term=reverse cterm=reverse 
-highlight CursorColumn ctermbg=7 term=reverse 
-highlight CursorLine term=underline cterm=underline 
-highlight ColorColumn ctermbg=224 term=reverse 
-highlight MatchParen ctermfg=0 ctermbg=7 term=reverse 
-highlight Comment ctermfg=4 term=bold 
-highlight Constant ctermfg=1 term=underline 
-highlight Special ctermfg=5 term=bold 
-highlight Identifier ctermfg=6 term=underline 
-highlight Statement ctermfg=130 term=bold 
-highlight PreProc ctermfg=5 term=underline 
-highlight Type ctermfg=2 term=underline 
-highlight Underlined ctermfg=5 term=underline cterm=underline 
-highlight Ignore ctermfg=15 
-highlight Error ctermfg=15 ctermbg=9 term=reverse 
-highlight Todo ctermfg=0 ctermbg=11 term=standout 
+highlight WarningMsg ctermfg=1 term=standout cterm=standout
+highlight WildMenu ctermfg=0 ctermbg=11 term=standout cterm=standout
+highlight Folded ctermfg=0 ctermbg=15 term=standout cterm=standout
+highlight FoldColumn ctermfg=0 ctermbg=15 term=standout cterm=standout
+highlight DiffAdd ctermfg=0 ctermbg=2 term=bold cterm=bold
+highlight DiffChange ctermfg=0 ctermbg=3 term=bold cterm=bold
+highlight DiffDelete ctermfg=0 ctermbg=6 term=bold cterm=bold
+highlight DiffText ctermfg=0 ctermbg=7 term=reverse,bold cterm=reverse,bold
+highlight SignColumn ctermbg=0 term=NONE cterm=NONE
+highlight Conceal ctermfg=7 ctermbg=242
+highlight SpellBad ctermbg=224 term=reverse cterm=reverse
+highlight SpellCap ctermbg=81 term=reverse cterm=reverse
+highlight SpellRare ctermbg=225 term=reverse cterm=reverse
+highlight SpellLocal ctermbg=14
+highlight Pmenu ctermfg=0 ctermbg=225
+highlight PmenuSel ctermfg=0 ctermbg=248
+highlight PmenuSbar ctermbg=248
+highlight PmenuThumb ctermbg=0
+highlight TabLine ctermfg=0 ctermbg=7 term=underline cterm=underline
+highlight TabLineSel term=bold cterm=bold
+highlight TabLineFill term=reverse cterm=reverse
+highlight CursorColumn ctermbg=7
+highlight CursorLine term=underline cterm=underline
+highlight ColorColumn ctermbg=224 term=reverse cterm=reverse
+highlight MatchParen ctermfg=0 ctermbg=7 term=reverse cterm=reverse
+highlight Comment ctermfg=4 term=bold cterm=bold
+highlight Constant ctermfg=1
+highlight Special ctermfg=5 term=bold cterm=bold
+highlight Identifier ctermfg=6
+highlight Statement ctermfg=130 term=bold cterm=bold
+highlight PreProc ctermfg=5
+highlight Type ctermfg=2
+highlight Underlined ctermfg=5 term=underline cterm=underline
+highlight Ignore ctermfg=15
+highlight Error ctermfg=15 ctermbg=9 term=reverse cterm=reverse
+highlight Todo ctermfg=0 ctermbg=11 term=standout cterm=standout
 highlight link Character Constant
 highlight link Number Constant
 highlight link Boolean Constant
@@ -76,4 +76,3 @@ highlight link SpecialChar Special
 highlight link Delimiter Special
 highlight link SpecialComment Special
 highlight link Debug Special
-
--- vault/vim/vault.ts
+++ vault/vim/vault.ts
@@ -18,6 +18,401 @@ export const VimCookBook = new CookBook({
       dst: "~/.vimrc",
     }),
   ],
+  setup: async (options: shell.CallOptions) => {
+    const highlights = [
+      {
+        "name": "SpecialKey",
+        "foreground": 4,
+        "options": ["bold"],
+      },
+      {
+        "name": "NonText",
+        "foreground": 12,
+        "options": ["bold"],
+      },
+      {
+        "name": "Directory",
+        "foreground": 4,
+        "options": ["bold"],
+      },
+      {
+        "name": "ErrorMsg",
+        "foreground": 15,
+        "background": 1,
+        "options": ["standout"],
+      },
+      {
+         "name": "IncSearch",
+          "options": ["reverse"],
+      },
+      {
+        "name": "Search",
+        "background": 11,
+        "options": ["reverse"],
+      },
+      {
+        "name": "MoreMsg",
+        "foreground": 2,
+        "options": ["bold"],
+      },
+      {
+        "name": "ModeMsg",
+        "options": ["bold"],
+      },
+      {
+        "name": "LineNr",
+        "foreground": 130,
+      },
+      {
+          "name": "CursorLineNr",
+          "foreground": 130,
+          "options": ["bold"],
+      },
+      {
+        "name": "Question",
+        "foreground": 2,
+        "options": ["standout"],
+      },
+      {
+        "name": "StatusLine",
+        "options": ["bold", "reverse"],
+      },
+      {
+        "name": "StatusLineNC",
+        "options": ["reverse"],
+      },
+      {
+        "name": "VertSplit",
+        "options": ["reverse"],
+      },
+      {
+        "name": "Title",
+        "foreground": 5,
+        "options": ["bold"],
+      },
+      {
+        "name": "Visual",
+        "background": 7,
+        "options": ["reverse"],
+      },
+      {
+        "name": "VisualNOS",
+        "options": ["bold", "underline"],
+      },
+      {
+        "name": "WarningMsg",
+        "foreground": 1,
+        "options": ["standout"],
+      },
+      {
+        "name": "WildMenu",
+        "foreground": 0,
+        "background": 11,
+        "options": ["standout"],
+      },
+      {
+        "name": "Folded",
+        "foreground": 0,
+        "background": 15,
+        "options": ["standout"],
+      },
+      {
+        "name": "FoldColumn",
+        "foreground": 0,
+        "background": 15,
+        "options": ["standout"],
+      },
+      {
+        "name": "DiffAdd",
+        "foreground": 0,
+        "background": 2,
+        "options": ["bold"],
+      },
+      {
+        "name": "DiffChange",
+        "foreground": 0,
+        "background": 3,
+        "options": ["bold"],
+      },
+      {
+        "name": "DiffDelete",
+        "foreground": 0,
+        "background": 6,
+        "options": ["bold"],
+      },
+      {
+        "name": "DiffText",
+        "foreground": 0,
+        "background": 7,
+        "options": ["reverse", "bold"],
+      },
+      {
+        "name": "SignColumn",
+        "background": 0,
+        "options": ["NONE"],
+      },
+      {
+        "name": "Conceal",
+        "foreground": 7,
+        "background": 242,
+      },
+      {
+        "name": "SpellBad",
+        "background": 224,
+        "options": ["reverse"],
+      },
+      {
+        "name": "SpellCap",
+        "background": 81,
+        "options": ["reverse"],
+      },
+      {
+        "name": "SpellRare",
+        "background": 225,
+        "options": ["reverse"],
+      },
+      {
+        "name": "SpellLocal",
+        "background": 14,
+      },
+      {
+        "name": "Pmenu",
+        "foreground": 0,
+        "background": 225,
+      },
+      {
+        "name": "PmenuSel",
+        "foreground": 0,
+        "background": 248,
+      },
+      {
+        "name": "PmenuSbar",
+        "background": 248,
+      },
+      {
+        "name": "PmenuThumb",
+        "background": 0,
+      },
+      {
+        "name": "TabLine",
+        "foreground": 0,
+        "background": 7,
+        "options": ["underline"],
+      },
+      {
+        "name": "TabLineSel",
+        "options": ["bold"],
+      },
+      {
+        "name": "TabLineFill",
+        "options": ["reverse"],
+      },
+      {
+        "name": "CursorColumn",
+        "background": 7,
+      },
+      {
+        "name": "CursorLine",
+        "options": ["underline"],
+      },
+      {
+        "name": "ColorColumn",
+        "background": 224,
+        "options": ["reverse"],
+      },
+      {
+        "name": "MatchParen",
+        "foreground": 0,
+        "background": 7,
+        "options": ["reverse"],
+      },
+      {
+        "name": "Comment",
+        "foreground": 4,
+        "options": ["bold"],
+      },
+      {
+        "name": "Constant",
+        "foreground": 1,
+      },
+      {
+        "name": "Special",
+        "foreground": 5,
+        "options": ["bold"],
+      },
+      {
+        "name": "Identifier",
+        "foreground": 6,
+      },
+      {
+        "name": "Statement",
+        "foreground": 130,
+        "options": ["bold"],
+      },
+      {
+        "name": "PreProc",
+        "foreground": 5,
+      },
+      {
+        "name": "Type",
+        "foreground": 2,
+      },
+      {
+        "name": "Underlined",
+        "foreground": 5,
+        "options": ["underline"],
+      },
+      {
+        "name": "Ignore",
+        "foreground": 15,
+      },
+      {
+        "name": "Error",
+        "foreground": 15,
+        "background": 9,
+        "options": ["reverse"],
+      },
+      {
+        "name": "Todo",
+        "foreground": 0,
+        "background": 11,
+        "options": ["standout"],
+      },
+    ];
+
+    const aliases = [
+      {
+        "name": "Character",
+        "link": "Constant",
+      },
+      {
+        "name": "Number",
+        "link": "Constant",
+      },
+      {
+        "name": "Boolean",
+        "link": "Constant",
+      },
+      {
+        "name": "Float",
+        "link": "Number",
+      },
+      {
+        "name": "Function",
+        "link": "Identifier",
+      },
+      {
+        "name": "Conditional",
+        "link": "Statement",
+      },
+      {
+        "name": "Repeat",
+        "link": "Statement",
+      },
+      {
+        "name": "Label",
+        "link": "Statement",
+      },
+      {
+        "name": "Operator",
+        "link": "Statement",
+      },
+      {
+        "name": "Keyword",
+        "link": "Statement",
+      },
+      {
+        "name": "Exception",
+        "link": "Statement",
+      },
+      {
+        "name": "Include",
+        "link": "PreProc",
+      },
+      {
+        "name": "Define",
+        "link": "PreProc",
+      },
+      {
+        "name": "Macro",
+        "link": "PreProc",
+      },
+      {
+        "name": "PreCondit",
+        "link": "PreProc",
+      },
+      {
+        "name": "StorageClass",
+        "link": "Type",
+      },
+      {
+        "name": "Structure",
+        "link": "Type",
+      },
+      {
+        "name": "Typedef",
+        "link": "Type",
+      },
+      {
+        "name": "Tag",
+        "link": "Special",
+      },
+      {
+        "name": "SpecialChar",
+        "link": "Special",
+      },
+      {
+        "name": "Delimiter",
+        "link": "Special",
+      },
+      {
+        "name": "SpecialComment",
+        "link": "Special",
+      },
+      {
+        "name": "Debug",
+        "link": "Special",
+      },
+    ];
+
+    const lines = [`let g:colors_name="muniere"`, ""];
+
+    for (const highlight of highlights) {
+      const chunks = ["highlight", highlight.name];
+
+      if (highlight.foreground !== undefined) {
+        chunks.push(`ctermfg=${highlight.foreground}`);
+      }
+      if (highlight.background !== undefined) {
+        chunks.push(`ctermbg=${highlight.background}`);
+      }
+      if (highlight.options) {
+        chunks.push(`term=${highlight.options.join(",")}`);
+        chunks.push(`cterm=${highlight.options.join(",")}`);
+      }
+
+      lines.push(chunks.join(" "));
+    }
+
+    for (const alias of aliases) {
+      const chunks = ["highlight", "link", alias.name, alias.link];
+
+      lines.push(chunks.join(" "));
+    }
+
+    const content = lines.join("\n");
+    const dst = ResLayout.vault().join("vim", "default", "colors", "muniere.vim");
+
+    options.logger?.debug(
+      `Create a file ${dst} with content:\n${content}`,
+    );
+
+    if (options.dryRun == true) {
+      return;
+    }
+
+    await Deno.writeTextFile(dst.toFileUrl(), content + "\n");
+  },
   activate: async (options: shell.CallOptions) => {
     const url = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim";
     const path = HomeLayout.config().join("vim/autoload/plug.vim");
