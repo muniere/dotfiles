77fc31e3e93c0d45a189c4fa3a47a8601f1b79bc
Hiromune Ito <muniere.fujiwara@gmail.com>
2024-12-30 23:25:27 +0900

    Configure setup action to generate theme for ghostty

--- vault/ghostty/vault.ts
+++ vault/ghostty/vault.ts
@@ -1,5 +1,8 @@
 import { HomeLayout, ResLayout } from "@dotfiles/lib/layout.ts";
 import { CookBook, PrefSpec } from "@dotfiles/lib/schema.ts";
+import * as theme from "@dotfiles/lib/theme.ts";
+import * as shell from "@dotfiles/lib/shell.ts";
+import { Eta } from "@eta-dev/eta";
 
 export const GhosttyCookBook = new CookBook({
   name: "GhosttyCookBook",
@@ -9,5 +12,21 @@ export const GhosttyCookBook = new CookBook({
       src: ".",
       dst: HomeLayout.config().join("ghostty/"),
     }),
-  ]
+  ],
+  setup: async (options: shell.CallOptions) => {
+    const src = ResLayout.vault().join("ghostty", "template", "themes", "muniere");
+    const dst = ResLayout.vault().join("ghostty", "default", "themes", "muniere");
+    const template = await Deno.readTextFile(src.toFileUrl());
+    const content = new Eta({autoTrim: [false, false]}).renderString(template, theme.Palette);
+
+    options.logger?.debug(
+      `Create a file ${dst} with content:\n${content}`,
+    );
+
+    if (options.dryRun == true) {
+      return;
+    }
+
+    await Deno.writeTextFile(dst.toFileUrl(), content);
+  },
 });
