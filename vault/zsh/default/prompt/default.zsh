4343cbfdace86e19e11df8faf721c7a0f7b92e4a
Hiromune Ito <muniere.fujiwara@gmail.com>
2025-01-06 23:09:28 +0900

    Use associative array to manage colors and symbols

--- vault/zsh/default/prompt/default.zsh
+++ vault/zsh/default/prompt/default.zsh
@@ -1,12 +1,22 @@
 () {
-    local user='%(?.%F{cyan}.%F{red})%n%f'
-    local host='%(?.%F{cyan}.%F{red})%m%f'
-    local path='%F{yellow}%~%f'
-    local venv='%F{white}${venv_info_msg}%f'
-    local vcs='%F{magenta}${vcs_info_msg_0_}%f'
-    local LF=$'\n'
+    local -A color=(
+        [red]='red'
+        [yellow]='yellow'
+        [cyan]='cyan'
+        [magenta]='magenta'
+        [white]='white'
+    )
+    local -A symbol=(
+        [LF]=$'\n'
+    )
 
-    export PROMPT="${user}@${host}${venv}: ${path}${LF}%(!.#.%%) "
+    local user="%(?.%F{$color[cyan]}.%F{$color[red]})%n%f"
+    local host="%(?.%F{$color[cyan]}.%F{$color[red]})%m%f"
+    local path="%F{$color[yellow]}%~%f"
+    local venv="%F{$color[white]}\${venv_info_msg}%f"
+    local vcs="%F{$color[magenta]}\${vcs_info_msg_0_}%f"
+
+    export PROMPT="${user}@${host}${venv}: ${path}${symbol[LF]}%(!.#.%%) "
     export RPROMPT="${vcs}"
     export PROMPT_PROVIDER=
 }
--- vault/zsh/default/prompt/powerline.zsh
+++ vault/zsh/default/prompt/powerline.zsh
@@ -1,26 +1,30 @@
 () {
-    local green='22'
-    local lime='154'
-    local rose='160'
-    local magenta='165'
-    local orange='166'
-    local black='232'
-    local white='255'
-    local ZH=''
-    local ZL=''
-    local LF=$'\n'
+    local -A color=(
+        [green]='22'
+        [lime]='154'
+        [rose]='160'
+        [magenta]='165'
+        [orange]='166'
+        [black]='232'
+        [white]='255'
+    )
+    local -A symbol=(
+        [ZH]=''
+        [ZL]=''
+        [LF]=$'\n'
+    )
 
     local info1_fmt=' %B%n@%m%b '
-    local info1_ok="%K{$lime}%F{$green}${info1_fmt}%f%k%K{$orange}%F{$lime}${ZL}%f%k"
-    local info1_ng="%K{$rose}%F{$white}${info1_fmt}%f%k%K{$orange}%F{$rose}${ZL}%f%k"
+    local info1_ok="%K{$color[lime]}%F{$color[green]}${info1_fmt}%f%k%K{$color[orange]}%F{$color[lime]}${symbol[ZL]}%f%k"
+    local info1_ng="%K{$color[rose]}%F{$color[white]}${info1_fmt}%f%k%K{$color[orange]}%F{$color[rose]}${symbol[ZL]}%f%k"
     local info1="%(?.${info1_ok}.${info1_ng})"
 
     local info2_fmt=' %B%~%b '
-    local info2="%K{$orange}%F{$white}${info2_fmt}%f%k%F{$orange}${ZL}%f%k"
+    local info2="%K{$color[orange]}%F{$color[white]}${info2_fmt}%f%k%F{$color[orange]}${symbol[ZL]}%f%k"
 
-    local vcs="%F{magenta}${vcs_info_msg_0_}%f"
+    local vcs="%F{$color[magenta]}\${vcs_info_msg_0_}%f"
 
-    export PROMPT="${info1}${info2}${LF}%(!.#.%%) "
+    export PROMPT="${info1}${info2}${symbol[LF]}%(!.#.%%) "
     export RPROMPT="${vcs}"
     export PROMPT_PROVIDER=
 }
