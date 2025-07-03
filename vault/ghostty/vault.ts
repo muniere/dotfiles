c02da892cd3b320a580472e86fe575b283fdf889
Hiromune Ito <muniere.fujiwara@gmail.com>
2025-07-03 12:41:57 +0900

    Split configuration files for ghostty

--- vault/ghostty/vault.ts
+++ vault/ghostty/vault.ts
@@ -15,19 +15,42 @@ export const GhosttyCookBook = new CookBook({
     }),
   ],
   setup: async (options: shell.CallOptions) => {
-    const src = ResLayout.vault().join("ghostty", "template", "themes", "muniere");
-    const dst = ResLayout.vault().join("ghostty", "default", "themes", "muniere");
-    const template = await Deno.readTextFile(src.toFileUrl());
-    const content = new Eta({ autoTrim: [false, false] }).renderString(template, theme.Palette);
+    // template
+    {
+      const src = ResLayout.vault().join("ghostty", "template", "themes", "muniere");
+      const dst = ResLayout.vault().join("ghostty", "default", "themes", "muniere");
+      const template = await Deno.readTextFile(src.toFileUrl());
+      const content = new Eta({ autoTrim: [false, false] }).renderString(template, theme.Palette);
 
-    options.logger?.debug(
-      `Create a file ${dst} with content:\n${content}`,
-    );
+      options.logger?.debug(
+        `Create a file ${dst} with content:\n${content}`,
+      );
 
-    if (options.dryRun == true) {
-      return;
+      if (options.dryRun !== true) {
+        await Deno.writeTextFile(dst.toFileUrl(), content);
+      }
     }
 
-    await Deno.writeTextFile(dst.toFileUrl(), content);
+    // config
+    {
+      const dir = HomeLayout.config().join("ghostty", "conf.d");
+      const file = dir.join("local.ghostrc");
+
+      if (options.dryRun !== true) {
+        await Deno.mkdir(dir.toFileUrl(), { recursive: true });
+      }
+
+      try {
+        if (options.dryRun !== true) {
+          await Deno.writeTextFile(file.toFileUrl(), "", { createNew: true });
+        }
+
+        options.logger?.debug(`Create a file ${file} with empty content`);
+      } catch (error) {
+        if (!(error instanceof Deno.errors.AlreadyExists)) {
+          throw error;
+        }
+      }
+    }
   },
 });
