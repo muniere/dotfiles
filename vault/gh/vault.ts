becf84b794e51a4d147dcfa7a7105c00cbcc7101
Hiromune Ito <muniere.fujiwara@gmail.com>
2026-02-28 00:26:23 +0900

    Use public gh-team instead of local one

--- vault/gh/vault.ts
+++ vault/gh/vault.ts
@@ -1,5 +1,6 @@
 import { HomeLayout, ResLayout } from "@dotfiles/lib/layout.ts";
 import { CookBook, PrefSpec } from "@dotfiles/lib/schema.ts";
+import * as shell from "@dotfiles/lib/shell.ts";
 
 export const GitHubCookBook = new CookBook({
   name: "GitHubCookBook",
@@ -10,4 +11,20 @@ export const GitHubCookBook = new CookBook({
       dst: HomeLayout.data().join("gh/"),
     }),
   ],
+  activate: async (options: shell.CallOptions) => {
+    const repos = [
+      "muniere/gh-team",
+    ];
+
+    const which = await shell.capture("which", ["gh"]);
+    const cmd = which.stdout.trim();
+    if (which.status.code !== 0 || !cmd) {
+      options.logger?.warn("gh not found on PATH yet. skip.");
+      return;
+    }
+
+    for (const repo of repos) {
+      await shell.call(cmd, ["extension", "install", repo], options);
+    }
+  },
 });
